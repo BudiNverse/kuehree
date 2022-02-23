597017580d758ea466fed44ed25c1157823154f7
Budi Syahiddin <me@inve.rs>
2022-02-23 19:32:20 +0800

    Added extensive clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,71 @@
+// Forbid warnings in release builds:
+#![cfg_attr(not(debug_assertions), deny(warnings))]
+#![forbid(unsafe_code)]
+#![warn(
+    clippy::all,
+    clippy::await_holding_lock,
+    clippy::char_lit_as_u8,
+    clippy::checked_conversions,
+    clippy::dbg_macro,
+    clippy::debug_assert_with_mut_call,
+    clippy::doc_markdown,
+    clippy::empty_enum,
+    clippy::enum_glob_use,
+    clippy::exit,
+    clippy::expl_impl_clone_on_copy,
+    clippy::explicit_deref_methods,
+    clippy::explicit_into_iter_loop,
+    clippy::fallible_impl_from,
+    clippy::filter_map_next,
+    clippy::float_cmp_const,
+    clippy::fn_params_excessive_bools,
+    clippy::if_let_mutex,
+    clippy::imprecise_flops,
+    clippy::inefficient_to_string,
+    clippy::invalid_upcast_comparisons,
+    clippy::large_types_passed_by_value,
+    clippy::let_unit_value,
+    clippy::linkedlist,
+    clippy::lossy_float_literal,
+    clippy::macro_use_imports,
+    clippy::manual_ok_or,
+    clippy::map_flatten,
+    clippy::match_on_vec_items,
+    clippy::match_same_arms,
+    clippy::match_wildcard_for_single_variants,
+    clippy::mem_forget,
+    clippy::mismatched_target_os,
+    clippy::missing_errors_doc,
+    clippy::missing_safety_doc,
+    clippy::mut_mut,
+    clippy::mutex_integer,
+    clippy::needless_borrow,
+    clippy::needless_continue,
+    clippy::needless_pass_by_value,
+    clippy::option_option,
+    clippy::path_buf_push_overwrite,
+    clippy::ptr_as_ptr,
+    clippy::avoid_breaking_exported_api,
+    clippy::ref_option_ref,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::same_functions_in_if_condition,
+    clippy::string_add_assign,
+    clippy::string_add,
+    clippy::string_lit_as_bytes,
+    clippy::string_to_string,
+    clippy::todo,
+    clippy::trait_duplication_in_bounds,
+    clippy::unimplemented,
+    clippy::unnested_or_patterns,
+    clippy::unused_self,
+    clippy::useless_transmute,
+    clippy::verbose_file_reads,
+    clippy::zero_sized_map_values,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms
+)]
+
 use num::Num;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
@@ -12,11 +80,20 @@ pub struct SumQueryFixed<T, const N: usize> {
     prefix_sum_array: [T; N],
 }
 
+/// This trait provides methods required for `SumQuery` types
 pub trait SumQuery {
     type InternalContainer;
     type InternalType: Copy;
 
+    /// Construct `Self`
+    ///
+    /// Algorithmic complexity: O(n)
     fn new(data: Self::InternalContainer) -> Self;
+
+    /// Query between start range and end range
+    ///
+    /// Negative querying is not implemented, hence end has to be greater
+    /// or equal to start
     fn query(&self, start: usize, end: usize) -> Self::InternalType;
 }
 
@@ -108,6 +185,7 @@ impl<T: Num + Copy> SumQuery for SumQueryVec<T> {
     }
 }
 
+#[cfg(test)]
 mod tests {
     use crate::{SumQuery, SumQueryFixed, SumQueryVec};
 
