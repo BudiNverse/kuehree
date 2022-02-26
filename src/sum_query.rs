e14b82b19e4f779b7fd12e36fe97adb040039200
Budi Syahiddin <me@inve.rs>
2022-02-26 15:49:09 +0800

    New API design

--- src/sum_query.rs
+++ src/sum_query.rs
@@ -1,5 +1,4 @@
 use std::{
-    marker::PhantomData,
     num::NonZeroUsize,
     ops::{Add, Index, Sub},
 };
@@ -7,24 +6,23 @@ use std::{
 use num::Zero;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct SumQuery<T, T3, T2 = Vec<T3>> {
-    data: T,
-    prefix_sum_array: T2,
-    _phantom: PhantomData<T3>,
+pub struct SumQuery<T: IntoIterator> {
+    prefix_sum_array: T,
 }
 
 /// This trait provides methods required for `SumQuery` types
-pub trait IndexableSumQuery<T, PrefixContainer = Vec<T>>
+pub trait IndexableSumQuery<T>
 where
-    T: Copy + Sub<Output = T>,
+    T: Copy + Sub<Output = T> + Add<Output = T>,
+    Self: Sized,
 {
-    type InternalContainer;
-    type PrefixSumContainer: Index<usize, Output = T>;
+    // type DataContainer: IntoIterator<Item = T>;
+    type PrefixSumContainer: IntoIterator<Item = T> + Index<usize, Output = T>;
 
     /// Construct `Self`
     ///
     /// Algorithmic complexity: O(n)
-    fn new(data: Self::InternalContainer) -> Self;
+    fn new(data: impl IntoIterator<Item = T>) -> Self;
 
     fn prefix_sum_array(&self) -> &Self::PrefixSumContainer;
 
@@ -51,76 +49,24 @@ where
         let prefix_sum_array = self.prefix_sum_array();
         prefix_sum_array[end.get()] - prefix_sum_array[(start.get()) - 1]
     }
-
-    /// Consumes itself and returns the internal parts
-    fn into_parts(self) -> (Self::InternalContainer, Self::PrefixSumContainer);
 }
 
-impl<T, const N: usize> IndexableSumQuery<T> for SumQuery<[T; N], T, [T; N]>
-where
-    T: Copy + Sub<Output = T> + Zero,
-{
-    type InternalContainer = [T; N];
-    type PrefixSumContainer = [T; N];
-
-    fn new(data: Self::InternalContainer) -> Self {
-        let mut prefix_sum_array = [T::zero(); N];
-        let mut idx = 0usize;
-
-        while idx < N {
-            if idx == 0 {
-                prefix_sum_array[idx] = data[idx];
-            } else {
-                prefix_sum_array[idx] = data[idx] + prefix_sum_array[idx - 1];
-            }
-            idx += 1;
-        }
-
-        Self {
-            data,
-            prefix_sum_array,
-            _phantom: PhantomData,
-        }
-    }
-
-    fn into_parts(self) -> (Self::PrefixSumContainer, Self::PrefixSumContainer) {
-        (self.data, self.prefix_sum_array)
-    }
-
-    fn prefix_sum_array(&self) -> &Self::PrefixSumContainer {
-        &self.prefix_sum_array
-    }
-}
-
-impl<T> IndexableSumQuery<T> for SumQuery<Vec<T>, T>
+impl<T> IndexableSumQuery<T> for SumQuery<Vec<T>>
 where
     T: Copy + Sub<Output = T> + Add<Output = T>,
 {
-    type InternalContainer = Vec<T>;
     type PrefixSumContainer = Vec<T>;
 
-    fn new(data: Self::InternalContainer) -> Self {
+    fn new(data: impl IntoIterator<Item = T>) -> Self {
         let mut prefix_sum_array = vec![];
-        let mut idx = 0usize;
-
-        while idx < data.len() {
+        for (idx, d) in data.into_iter().enumerate() {
             if idx == 0 {
-                prefix_sum_array.push(data[idx]);
+                prefix_sum_array.push(d);
             } else {
-                prefix_sum_array.push(data[idx] + prefix_sum_array[idx - 1]);
+                prefix_sum_array.push(d + prefix_sum_array[idx - 1]);
             }
-            idx += 1;
         }
-
-        Self {
-            data,
-            prefix_sum_array,
-            _phantom: PhantomData,
-        }
-    }
-
-    fn into_parts(self) -> (Self::PrefixSumContainer, Self::PrefixSumContainer) {
-        (self.data, self.prefix_sum_array)
+        Self { prefix_sum_array }
     }
 
     fn prefix_sum_array(&self) -> &Self::PrefixSumContainer {
@@ -128,81 +74,72 @@ where
     }
 }
 
-impl<'a, T> IndexableSumQuery<T> for SumQuery<&'a [T], T>
-where
-    T: Copy + Sub<Output = T> + Add<Output = T>,
-{
-    type InternalContainer = &'a [T];
-    type PrefixSumContainer = Vec<T>;
-
-    fn new(data: Self::InternalContainer) -> Self {
-        let mut prefix_sum_array = vec![];
-        let mut idx = 0usize;
-
-        while idx < data.len() {
-            if idx == 0 {
-                prefix_sum_array.push(data[idx]);
-            } else {
-                prefix_sum_array.push(data[idx] + prefix_sum_array[idx - 1]);
-            }
-            idx += 1;
-        }
-
-        Self {
-            data,
-            prefix_sum_array,
-            _phantom: PhantomData,
-        }
-    }
-
-    fn into_parts(self) -> (Self::InternalContainer, Self::PrefixSumContainer) {
-        (self.data, self.prefix_sum_array)
-    }
-
-    fn prefix_sum_array(&self) -> &Self::PrefixSumContainer {
-        &self.prefix_sum_array
-    }
-}
-
-impl<'a, T, const N: usize> IndexableSumQuery<T> for SumQuery<&'a [T; N], T, [T; N]>
+impl<'a, T, const N: usize> IndexableSumQuery<T> for SumQuery<[T; N]>
 where
     T: Copy + Sub<Output = T> + Add<Output = T> + Zero,
 {
-    type InternalContainer = &'a [T; N];
     type PrefixSumContainer = [T; N];
 
-    fn new(data: Self::InternalContainer) -> Self {
+    fn new(data: impl IntoIterator<Item = T>) -> Self {
         let mut prefix_sum_array = [T::zero(); N];
-        let mut idx = 0usize;
-
-        while idx < N {
+        for (idx, d) in data.into_iter().enumerate() {
             if idx == 0 {
-                prefix_sum_array[idx] = data[idx];
+                prefix_sum_array[idx] = d;
             } else {
-                prefix_sum_array[idx] = data[idx] + prefix_sum_array[idx - 1];
+                prefix_sum_array[idx] = d + prefix_sum_array[idx - 1];
             }
-            idx += 1;
         }
-
-        Self {
-            data,
-            prefix_sum_array,
-            _phantom: PhantomData,
-        }
-    }
-
-    fn into_parts(self) -> (Self::InternalContainer, Self::PrefixSumContainer) {
-        (self.data, self.prefix_sum_array)
+        Self { prefix_sum_array }
     }
 
     fn prefix_sum_array(&self) -> &Self::PrefixSumContainer {
         &self.prefix_sum_array
+    }
+}
+
+impl<T, T2> From<T2> for SumQuery<Vec<T>>
+where
+    T2: AsRef<[T]>,
+    T: Copy + Add<Output = T>,
+{
+    fn from(data: T2) -> Self {
+        let data = data.as_ref();
+        let mut prefix_sum_array = vec![];
+        let mut idx = 0usize;
+        for d in data {
+            if idx == 0 {
+                prefix_sum_array.push(*d);
+            } else {
+                prefix_sum_array.push(*d + prefix_sum_array[idx - 1]);
+            }
+            idx += 1;
+        }
+        Self { prefix_sum_array }
+    }
+}
+
+impl<T, T2, const N: usize> From<T2> for SumQuery<[T; N]>
+where
+    T2: AsRef<[T]>,
+    T: Copy + Add<Output = T> + Zero,
+{
+    fn from(data: T2) -> Self {
+        let data = data.as_ref();
+        let mut prefix_sum_array = [T::zero(); N];
+        for (idx, d) in data.into_iter().enumerate() {
+            if idx == 0 {
+                prefix_sum_array[idx] = *d;
+            } else {
+                prefix_sum_array[idx] = *d + prefix_sum_array[idx - 1];
+            }
+        }
+        Self { prefix_sum_array }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use std::mem::size_of;
+    use std::{collections::HashMap, mem::size_of};
 
     use super::*;
 
@@ -212,15 +149,18 @@ mod test {
         let data2 = vec![123u32];
         let data3 = vec![123u32];
         let data3 = data3.as_slice();
+        let hm = HashMap::<u32, u32>::new();
 
-        let _sum = SumQuery::<[_; 1], _, _>::new(data);
-        let _sum = SumQuery::<Vec<_>, _>::new(data2);
-        let _sum = SumQuery::<&[_], _>::new(data3);
+        let _sum = SumQuery::<[_; 1]>::new(data);
+        let _sum = SumQuery::<Vec<_>>::new(data2);
+        let _sum = SumQuery::<Vec<_>>::from(data3);
     }
 
     #[test]
     fn test_query_u32_vec() {
-        let sum = SumQuery::<[_; 8], _, _>::new([1, 3, 4, 8, 6, 1, 4, 2]);
+        let data = vec![1, 3, 4, 8, 6, 1, 4, 2];
+        let sum = SumQuery::<Vec<_>>::from(&data);
+        data[0];
 
         let results = [
             (sum.query(3, 6), 19u32),
@@ -238,7 +178,7 @@ mod test {
 
     #[test]
     fn test_query_u64() {
-        let sum = SumQuery::<[_; 8], _, _>::new([1, 3, 4, 8, 6, 1, 4, 2]);
+        let sum = SumQuery::<[_; 8]>::new([1, 3, 4, 8, 6, 1, 4, 2]);
 
         let results = [
             (sum.query(3, 6), 19u64),
@@ -256,7 +196,7 @@ mod test {
 
     #[test]
     fn test_query_i8() {
-        let sum = SumQuery::<[_; 8], _, _>::new([1, 3, 4, 8, 6, 1, 4, 2]);
+        let sum = SumQuery::<[_; 8]>::new([1, 3, 4, 8, 6, 1, 4, 2]);
 
         let results = [
             (sum.query(3, 6), 19i8),
@@ -274,7 +214,7 @@ mod test {
 
     #[test]
     fn test_query_f32() {
-        let sum = SumQuery::<[_; 8], _, _>::new([1.0f32, 3.0, 4.0, 8.0, 6.0, 1.0, 4.0, 2.0]);
+        let sum = SumQuery::<[_; 8]>::new([1.0f32, 3.0, 4.0, 8.0, 6.0, 1.0, 4.0, 2.0]);
 
         let results = [
             (sum.query(3, 6), 19.0),
@@ -292,8 +232,9 @@ mod test {
 
     #[test]
     fn test_query_slice() {
-        let slice = [1, 3, 4, 8, 6, 1, 4, 2];
-        let sum = SumQuery::<&[_; 8], _, _>::new(&slice);
+        let slice = [1u32, 3, 4, 8, 6, 1, 4, 2];
+        let slice = &slice;
+        let sum = SumQuery::<[_; 8]>::from(slice);
 
         let results = [
             (sum.query(3, 6), 19u32),
@@ -313,7 +254,7 @@ mod test {
     fn test_query_slice_vec() {
         let slice = vec![1u32, 3, 4, 8, 6, 1, 4, 2];
         let slice_ref: &[_] = slice.as_ref();
-        let sum = SumQuery::<&[_], _>::new(slice_ref);
+        let sum = SumQuery::<Vec<_>>::from(slice_ref);
 
         let results = [
             (sum.query(3, 6), 19u32),
@@ -332,18 +273,12 @@ mod test {
     #[ignore]
     #[test]
     fn test_sz() {
-        let sz = size_of::<SumQuery<&[u32; 256], u32, [u32; 256]>>();
-        let sz_2 = size_of::<SumQuery<&[u32; 256], u32>>();
-        let sz_3 = size_of::<SumQuery<&[u32], u32>>();
-        let sz_4 = size_of::<Box<SumQuery<[u32; 256], u32, [u32; 256]>>>();
-        let sz_5 = size_of::<SumQuery<Vec<u32>, u32>>();
-        let sz_6 = size_of::<SumQuery<Box<[u32]>, u32, Box<[u32]>>>();
-        
+        let sz = size_of::<SumQuery<[u32; 256]>>();
+        let sz_4 = size_of::<Box<SumQuery<[u32; 256]>>>();
+        let sz_5 = size_of::<SumQuery<Vec<u32>>>();
+
         println!("{sz}");
-        println!("{sz_2}");
-        println!("{sz_3}");
         println!("{sz_4}");
         println!("{sz_5}");
-        println!("{sz_6}");
     }
 }
